41bbcb185ffc4a50c41971ae41c2c8db173ee261
Raf <socozy7210@gmail.com>
2023-05-30 16:55:59 +0900

    refactor

--- runtime/infrablockspace/src/weights/mod.rs
+++ runtime/infrablockspace/src/weights/mod.rs
@@ -15,7 +15,6 @@
 
 //! A list of the different weight modules for our runtime.
 
-pub mod frame_election_provider_support;
 pub mod frame_system;
 pub mod pallet_balances;
 pub mod pallet_bounties;
